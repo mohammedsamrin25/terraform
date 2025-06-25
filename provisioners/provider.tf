beea5162645feb221cd6b8ac1ef1ff5808346d7c
samrin <samreenshamaa@gmail.com>
2025-06-25 06:47:42 +0530

    terraform script

--- provisioners/provider.tf
+++ provisioners/provider.tf
@@ -5,6 +5,14 @@ terraform {
       version = "5.98.0"
     }
   }
+
+  backend "s3" {
+    bucket = "summu-remote-state"
+    key    = "provisioners"
+    region = "us-east-1"
+    encrypt        = true
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
---
+++ terraform-multi-env/tfvars/prod/backend.tf
@@ -0,0 +1,5 @@
+bucket = "summu-remote-state-prod"
+key    = "tfvars-demo"
+region = "us-east-1"
+encrypt        = true
+use_lockfile = true
